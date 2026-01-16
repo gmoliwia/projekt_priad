e0be25bf48622af44e41fafe784df6b64f48fabf
oliwia <oliwia@MacBook-Air-oliwia.local>
2026-01-16 14:35:29 +0100

    Początek

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+import nltk
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
